e9c461fd7f6060b173fafe800436054af6a30b4f
Bilalhassan679 <ftk4321@gmail.com>
2022-01-25 16:30:30 +0500

    1st Commit (I will follow 3 line agora video in youtube ) use dependency agora uikit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  AgoraClient client = AgoraClient(
+      agoraConnectionData: AgoraConnectionData(
+          appId: '832bc958d399476c941d6457b25c4d5f', channelName: 'test'),
+      enabledPermission: [Permission.camera, Permission.microphone]);
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Stack(children: [
+          AgoraVideoViewer(
+            client: client,
+            layoutType: Layout.floating,
+          ),
+          AgoraVideoButtons(client: client,
+            autoHideButtons: true,
+            autoHideButtonTime: 10,
+          )
+        ]));
+  }
+}
